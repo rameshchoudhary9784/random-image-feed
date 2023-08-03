af200e42331bcc1383e5d4f16c84950193f21dff
rameshk978433@gmail.com <rameshk978433@gmail.com>
2023-08-03 17:43:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const container = document.querySelector('.container')
+const URL = 'https://source.unsplash.com/random/'
+const rows = 10
+
+for(var i = 0; i < rows * 3; i++)
+{
+    const img = document.createElement('img')
+    img.src = `${URL}${getRandomSize()}`
+    container.appendChild(img)
+}
+
+console.log(getRandomSize())
+function getRandomSize()
+{
+    return `${getRondomNumber()}x${getRondomNumber()}` 
+}
+
+function getRondomNumber() {
+    return Math.floor(Math.random() * 10) + 300
+}
